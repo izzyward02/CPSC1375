f2d3fbcd2d39335bb5d1be70ee05a622c8f4dc13
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-26 23:58:04 -0500

    Create Lvl1.2_PowerPlay6.cpp

---
+++ Lvl1.2_PowerPlay6.cpp
@@ -0,0 +1,20 @@
+/* Izzy Ward    CPSC 1375    May 26, 2023
+*  INSTRUCTIONS:
+*   Ask the user for integer values for two x,y coordinates
+*   Calculate the distance between the two points
+*   Display the distance
+*   IMPORTANT FORMULA:  sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2)) */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Submit two whole number coordinates." << std::endl;
+    int x1{};
+    int y1{};
+    int x2{};
+    int y2();
+  std::cin >> std::
+  return 0;
+}
