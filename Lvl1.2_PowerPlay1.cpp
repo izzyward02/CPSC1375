0a9007db17ada9ae8b7747ac2de3dc57dc26d96e
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-26 23:36:52 -0500

    Update Lvl1.2_PowerPlay1.cpp

--- Lvl1.2_PowerPlay1.cpp
+++ Lvl1.2_PowerPlay1.cpp
@@ -1,7 +1,19 @@
+/* Izzy Ward    CPSC 1375    May 26, 2023
+*  INSTRUCTIONS:
+*   Write a program that asks the user for his/her name
+*   and greet them with their name  */
+
 #include <iostream>
 #include <string>
 
 int main()
 {
-  return 0;
+  std::cout << "Hello! What is your name?" << std::endl;
+  
+  std::string friend_name;
+  string::cin >> friend_name;
+  
+  std::cout << "Nice to meet you, " << friend_name << "!" << std::endl;
+  
+    return 0;
 }
