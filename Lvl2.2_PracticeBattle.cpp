4d5261a77c140f051096f2a0a63233a65c4c47aa
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-29 09:36:22 -0500

    Update Lvl2.2_PracticeBattle.cpp

--- Lvl2.2_PracticeBattle.cpp
+++ Lvl2.2_PracticeBattle.cpp
@@ -1,1 +1,17 @@
-// 
+/* Author:  Izzy Ward     CPSC 1375
+*  Date: May 29, 2023
+*   INSTRUCTIONS: Write a simple struct with at least two members that are fundamental types
+*   Make up an enum that is appropriate for this struct and include it in the code
+*   Use enum class as opposed to old fashioned enum.  */
+
+#include <iostream>
+#include <string>
+
+struct Pets
+{
+  enum class Identification {species, color, name};
+  
+  std::string petSpecies;
+  std::string petColor;
+  std::string petName;
+}
