a02339daa85e27cb383dacb5a41f3fc70adeedbd
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-30 17:55:56 -0500

    Create Lvl2.2_PowerPlay2.cpp

---
+++ Lvl2.2_PowerPlay2.cpp
@@ -0,0 +1,21 @@
+/* Izzy Ward    CPSC 1375   May 30, 2023
+*  INSTRUCTIONS:
+*   Ask for two integers
+*   Ask whether to (a)dd, (s)ubtract, (m)ultiply, or (d)ivide the numbers
+*   Double check you are not dividing by zero
+*   Use a 'switch' case for choosing the operation  */
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+
+int main()
+{
+  std::cout << "Choose two numbers: \n";
+    std::string num1;
+    std::string num2;
+  std::cin >> num1 >> num2;
+  
+  
+  return 0;
+}
