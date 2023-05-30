a60f7a67ed726c83af95dd4628e0c118a5c0b8f5
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-30 18:56:26 -0500

    Update Lvl2.2_PowerPlay4.cpp

--- Lvl2.2_PowerPlay4.cpp
+++ Lvl2.2_PowerPlay4.cpp
@@ -16,7 +16,7 @@ int main()
   enum class Number_Game { win, lose };
   
   std::tuple<int, int, int, int, int> nums;
-  auto& [win, lose] = nums;
+  auto& [num1, num2, num3, num4, num5] = nums;
   
   std::cout << "You will choose five integers. First number: ";
     std::cin >> num1;
@@ -29,37 +29,10 @@ int main()
   std::cout << "Fifth number: ";
     std::cin >> num5;
   
-  std::cout << "Which operation would you like to use? \n"
-    << " a = Addition\n"
-    << " s = Subtraction\n"
-    << " m = Multiplication\n"
-    << " d = Division\n";
+  if ((num1 + num2 + num3 + num4 + num5) > 100)
+    std::cout << "YOU WIN!!!!! \n";
+  else
+    std::cout << "YOU LOSE :( \n";
   
-  int userChoice{};
-  std::cin >> userChoice;
-  
-  userChoice--;
-  switch ((Calculate)userChoice)
-  {
-    case Calculate::a:
-      std::get <a> (num1 + num2);
-      break;
-    case Calculate::s:
-      std::get <s> (num1 - num2);
-      break;
-    case Calculate::m:
-      std::get <m> (num1 * num2);
-      break;
-    case Calculate::d:
-      if num2 != 0
-        std::get <d> (num1 / num2);
-      else
-        std::cout << "Your denominator cannot be zero! \n"
-      break;
-    default:
-      break;
-  }
-  float total{ (a + 0f) + (s + 0f) + (m + 0f) + (d+ 0f) };
-  std::cout << "Your total is: " << std::fixed << std::setprecision(0) << total 
   return 0;
 }
