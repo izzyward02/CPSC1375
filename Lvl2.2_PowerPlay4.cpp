eb508b115a8065b3510c906ca19b10eb53c78e5f
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-30 18:46:13 -0500

    Create Lvl2.2_PowerPlay4.cpp

---
+++ Lvl2.2_PowerPlay4.cpp
@@ -0,0 +1,19 @@
+/*  Izzy Ward   CPSC 1375   May 30, 2023
+*   INSTRUCTIONS:
+*     Ask the user for 5 integers
+*     Add them up
+*     If the sum is greater than 100, output "YOU WIN!!!!!"
+*     Otherwise, output "YOU LOSE :("
+*     NOTE: only 2 variables are needed for this problem  */
+
+#include <iostream>
+#include <string>
+#include <tuple>
+#include <iomanip>
+
+enum class Number_Game
+int main()
+{
+  
+  return 0;
+}
