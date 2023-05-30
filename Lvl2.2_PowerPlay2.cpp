49d2afd2da2c2707a8a31731797704ef389e15b1
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-30 18:15:24 -0500

    Update Lvl2.2_PowerPlay2.cpp

--- Lvl2.2_PowerPlay2.cpp
+++ Lvl2.2_PowerPlay2.cpp
@@ -7,35 +7,52 @@
 
 #include <iostream>
 #include <string>
+#include <tuple>
 #include <iomanip>
 
 int main()
 {
-  std::cout << "Choose two numbers: \n";
-    std::string num1;
-    std::string num2;
-  std::cin >> num1 >> num2;
+  enum class Calculate { a, s, m, d };
+  
+  std::tuple<int, int> nums;
+  auto& [num1, num2] = nums;
+  
+  std::cout << "Pick your first number: ";
+    std::cin >> num1;
+  std::cout << "Pick your second integer: ";
+    std::cin >> num2;
+  
+  std::cout << "Which operation would you like to use? \n"
+    << " a = Addition\n"
+    << " s = Subtraction\n"
+    << " m = Multiplication\n"
+    << " d = Division\n";
   
   int userChoice{};
   std::cin >> userChoice;
   
   userChoice--;
-  switch ((Coin)userChoice)
+  switch ((Calculate)userChoice)
   {
-    case Coin::penny:
-      std::get <0> (purse)++;
+    case Calculate::a:
+      std::get <a> (num1 + num2);
       break;
-    case Coin::nickel:
-      std::get <1> (purse)++;
+    case Calculate::s:
+      std::get <s> (num1 - num2);
       break;
-    case Coin::dime:
-      std::get <2> (purse)++;
+    case Calculate::m:
+      std::get <m> (num1 * num2);
       break;
-    case Coin::quarter:
-      std::get <3> (purse)++;
+    case Calculate::d:
+      if num2 != 0
+        std::get <d> (num1 / num2);
+      else
+        std::cout << "Your denominator cannot be zero! \n"
       break;
     default:
       break;
   }
-  return 0;
+  float total{ (a + 0f) + (s + 0f) + (m + 0f) + (d+ 0f) };
+  std::cout << "Your total is: " << std::fixed << std::setprecision(0) << total << std::endl;
+    system("PAUSE");
 }
