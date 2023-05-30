0687334b7a50c1a47575da436dcf138a16f38ad9
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-05-30 17:33:26 -0500

    Update Lvl2.2_PowerPlay1.cpp

--- Lvl2.2_PowerPlay1.cpp
+++ Lvl2.2_PowerPlay1.cpp
@@ -1,1 +1,26 @@
+/*  Izzy Ward   CPSC 1375   May 30, 2023
+* INSTRUCTIONS:
+*   Write a program that asks the user for their name
+*   Only greet them if their name is 'Alice' or 'Bob'
+*   Otherwise, give an adequate, generic response */
 
+#include <iostream>
+#include <string>
+
+int main()
+{
+  std::cout << "Hello! What is your name?" << '\n';
+    std::string friend_a{"Alice"};
+    std::string friend_b{"Bob"};
+    std::string your_name;
+  std::cin >> your_name;
+  
+  if (your_name == friend_a)
+    std::cout << "Hello, Alice!" << '\n';
+  else if (your_name == friend_b)
+    std::cout << "Hello, Bob!" << '\n';
+  else
+    std::cout << "Hello, friend!" << '\n'
+    
+  return 0;
+}
