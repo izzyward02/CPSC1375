a122492669a3dbc22e2b22db6264466b16462381
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-02 21:59:27 -0500

    Create Lvl2.3_PowerPlay4.cpp

---
+++ Lvl2.3_PowerPlay4.cpp
@@ -0,0 +1,28 @@
+//  Izzy Ward   CPSC 1375   June 2, 2023
+//    INSTRUCTIONS:
+//      Define an 'address' struct with the following members:
+//        int streetNumber;
+//        string street;
+//        string city;
+//        string state;
+//        int zipcode;
+//      Create an array called 'block' that holds 3 addresses
+//      Ask the user for the 3 addresses
+//      Don't create any temporaries outside the array
+
+#include <iostream>
+#include <string>
+
+struct Address
+{
+  int streetNumber;
+  string street;
+  string city;
+  string state;
+  int zipcode;
+};
+
+int main()
+{
+  return 0;
+}
