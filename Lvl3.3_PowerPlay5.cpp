44c952f53e3f60781370e1b22ba876d8205c835e
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-12 23:45:23 -0500

    Create Lvl3.3_PowerPlay5.cpp

---
+++ Lvl3.3_PowerPlay5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <random>
+
+int main()
+{
+  const int nums = 100;
+
+  std::default_random_engine generator;
+  std::bernoulli_distribution distribution(0.9); //distribution of trues
+
+  int count = 0;  //count number of trues
+
+  for (int i = 0; i < nums; ++i) 
+    if (distribution(generator)) ++count;
+      std::cout << "bernoulli_distribution (0.9) x 100:" << std::endl;
+      std::cout << "true:  " << count << std::endl;
+      std::cout << "false: " << nums - count << std::endl;
+
+  return 0;
+}
