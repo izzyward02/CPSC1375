9aa8f4d4aa4d3ded31039fd9517b5354ec663885
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-15 15:16:01 -0500

    Create Lvl4.1_PowerPlay2.cpp

---
+++ Lvl4.1_PowerPlay2.cpp
@@ -0,0 +1,8 @@
+/*    Izzy Ward   CPSC 1375   June 15, 2023
+*       INSTRUCTIONS:
+*         Create a 'Polygon' class with members...
+*         (public) 'Coordinates' struct with members float x; and flost y;
+*         void addCoordinate(Coordinate point);, void addCoordinate(); & float calculatePeremeter();
+*         (private) vector<Coordinate> points; & float calculateDistance(Coordiante point1, Coordinate point2)
+*         int main() should include...
+*         Create a polygon called 'octagon', add 8 coordinates via user input, and output the perimeter   */
