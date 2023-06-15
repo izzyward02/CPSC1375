1d20b561987e6fa0a2b9f6d6e9f635dc541cfd7d
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-15 15:11:39 -0500

    Create Lvl4.1_PowerPlay1.cpp

---
+++ Lvl4.1_PowerPlay1.cpp
@@ -0,0 +1,10 @@
+/*  Izzy Ward   CPSC 1375   June 15, 2023
+*     INSTRUCTIONS:
+*       Reuse the Card struct from prev. PowerPlay
+*       Create a 'Deck' class with members... (public) void shuffleDeck();, void shuffleEntireDeck();, 
+*       and Card dealCard();  ///   (private) vector<Card> drawPile;
+*       Create a 'Hand' class with members... (public) void drawCard(Card drawnCard); & void revealHand();
+*       (private) vector<Card> currentHand;
+*       Create a 'Player' class with members... (public) void drawCard(Card drawnCard);, void call();,
+*       void raise(int & pot), and void revealHand();   ///   (private) Hand currentHand; & int money;    */
+
