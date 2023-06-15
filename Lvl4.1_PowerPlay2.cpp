7ca353dd23ed6239ae6e7162ce8bbd1a41634bfb
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-15 16:13:16 -0500

    Update Lvl4.1_PowerPlay2.cpp

--- Lvl4.1_PowerPlay2.cpp
+++ Lvl4.1_PowerPlay2.cpp
@@ -15,4 +15,62 @@ class Polygon
 {
   public:
     struct Coordinate
+    {
+      float x{ 0.0f };
+      float y{ 0.0f };
+    };
+    void addCoordinate(Coordinate point);
+    void addCoordinate();
+    float calculatePerimeter();
+
+  private:
+    std::vector<Coordinate> points;
+    float calculateDistance(Coordinate point1, Coordinate point2);
+};
+
+int main()
+{
+  Polygon octagon;
+  std::cout << "Please enter 8 coordinates: " << std::endl;
+  for (int i{ 0 }; i < 8; i++)
+  {
+    octagon.addCoordinate();
+  }
+  std::cout << "The perimeter for your polygon is: " << octagon.calculatePerimeter();
+
+  return 0;
 }
+
+void Polygon::addCoordinate(Coordinatepoint)
+{
+  points.push_back(point);
+}
+
+void Polygon::addCoordinate()
+{
+  Coordinate temp;
+  std::cout << "Please enter your x and y values: " << std::endl;
+  std::cin >> temp.x >> temp.y;
+  addCoordinate(temp);
+}
+
+float Polygon::calculatePerimeter()
+{
+  if (points.size() > 2)
+  {
+    float perimeter{ 0.0f };
+    for (int i{ 0 }; i < points.size() - 1; i++)
+    {
+      perimeter += calcualteDistance(points.at(i), points.at(i + 1));
+    }
+    perimeter += calculateDistance(points.front(), points.back());
+    return perimeter;
+  }
+  return 0.0f;
+}
+
+float Polygon::calculateDistance(Coordinate point1, Coordinate point2)
+{
+  return sqrt((point1.x - point2.x) * (point1.x - point2.x) + (point1.y - point2.y) * (point1.y - point2.y));
+}
+
