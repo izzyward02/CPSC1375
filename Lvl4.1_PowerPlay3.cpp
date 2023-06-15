d2aef76368b22b5da83a5fcec6e21a903fe236f9
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-15 15:18:44 -0500

    Create Lvl4.1_PowerPlay3.cpp

---
+++ Lvl4.1_PowerPlay3.cpp
@@ -0,0 +1,8 @@
+/*    Izzy Ward   CPSC 1375   June 15, 2023
+*       INSTRUCTIONS:
+*         Create an 'Atom' class with members... (private) int number, string name, string abbreviation,
+*         and float weight
+*         Include getters and setters for all four members
+*         In int main()... Create a vector<Atom> water
+*         Push two Hydrogen atoms and an Oxygen atom onto the vector    */
+
