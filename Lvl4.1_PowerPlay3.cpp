74fa78e1bf9331f2f0fb2b507797c43cde0b5ee2
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-15 23:49:37 -0500

    Update Lvl4.1_PowerPlay3.cpp

--- Lvl4.1_PowerPlay3.cpp
+++ Lvl4.1_PowerPlay3.cpp
@@ -6,3 +6,46 @@
 *         In int main()... Create a vector<Atom> water
 *         Push two Hydrogen atoms and an Oxygen atom onto the vector    */
 
+#include <iostream>
+#include <vector>
+#include <math.h>
+
+class Atom
+{
+  private:
+    int number;
+    std::string name;
+    std::string abbreviation;
+    float weight;
+
+  public:
+    // Setters
+    void setNumber(int num)
+      number = num;
+    void setName(std::string name)
+      name = ID;
+    void setAbbreviation(std::string abbreviation)
+      abbreviation = abrv;
+    void setWeight(float weight)
+      weight = w;
+    // Getters
+    int getNumber()
+      return number;
+    std::string getName()
+      return name;
+    std::string getAbbreviation()
+      return abbreviation;
+    float getWeight()
+      return weight;
+};
+
+int main()
+{
+  std::vector<Atom> atoms;
+ // switch(atoms)
+   // case Atom::
+  
+  //NOTE TO MR. ORME: program is incomplete due to time constraints (spent too much time on Quest 3)
+  return 0;
+}
+
