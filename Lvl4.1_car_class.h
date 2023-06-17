1fecf0ca4b921da4673194e210dcd459acc62cc0
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-16 23:34:51 -0500

    Update Lvl4.1_car_class.h

--- Lvl4.1_car_class.h
+++ Lvl4.1_car_class.h
@@ -1,7 +1,11 @@
-  //HEADER FILE FOR LVL 4.1 PRACTICE BATTLE
+//HEADER FILE FOR LVL 4.1 PRACTICE BATTLE
+#ifndef CAR_CLASS_H
+#define CAR_CLASS_H
+#pragma once
 
 class Car
 {
   public:
     
 };
+#endif 
