973e812190c5a42a29fc641757b765152b7b218a
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-16 23:38:11 -0500

    Update Lvl4.1_car_class.h

--- Lvl4.1_car_class.h
+++ Lvl4.1_car_class.h
@@ -1,4 +1,7 @@
 //HEADER FILE FOR LVL 4.1 PRACTICE BATTLE
+#include <iostream>
+#include <vector>
+#include <string>
 #ifndef CAR_CLASS_H
 #define CAR_CLASS_H
 #pragma once
@@ -6,6 +9,30 @@
 class Car
 {
   public:
+    Car();  //Default Constructor
+    
+    //Setters
+    void setNumWheels(int wheels)
+      numWheels = wheels;
+    void numPassengers(int pass)
+      numPassengers = pass;
+    void year(int y)
+      year = y;
+    void make(std::string mk)
+      make = mk;
+    void model(std::string md)
+      model = md;
+    //Getters
+    int getNumWheels()
+      return numWheels;
+    int getNumPassengers()
+      return numPassengers;
+    int getYear()
+      return year;
+    std::string getMake()
+      return make;
+    std::string getModel()
+      return model;
     
 };
 #endif 
