a32f73ac4fe3daf3558539f6dd5804fd7b55c599
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-18 15:57:54 -0500

    Create Lvl4.2_PowerPlay1.cpp

---
+++ Lvl4.2_PowerPlay1.cpp
@@ -0,0 +1,19 @@
+/*  Izzy Ward   CPSC 1375   June 18, 2023
+*     INSTRUCTIONS:
+*       Copy & paste prev. PowerPlay for deck of cards & move all classes to separate .h and .cpp files
+*       Deck Class modification: Constructor that calls 'shuffleEntireDeck()'
+*       Hand Class modification: nothing
+*       Player Class modification: Conversion constructor that takes an int ante & a int& pot,
+*         subtracts ante from the money and adds it to the pot. Also asks the player how much money they have.
+*       Enum class "Action{ call, fold, bid };", private member Action currentAction{ Action::bid };, 
+*       Call method, now sets currentAction;, fold method, sets currentAction;
+*       raise(int& pot) asks for bid, removes from money, adds to pot, sets action.
+*       Use 'void takeTurn(int& pot);', 'bool isBidding() const;', and 'void newRound();' */
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+  return 0;
+}
