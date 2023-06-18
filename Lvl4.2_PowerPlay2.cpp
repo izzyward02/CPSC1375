2b9b46f3fd9594fc1f55f92dce22673c34e6c99c
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-18 16:01:49 -0500

    Create Lvl4.2_PowerPlay2.cpp

---
+++ Lvl4.2_PowerPlay2.cpp
@@ -0,0 +1,6 @@
+/*  Izzy Ward   CPSC 1375   June 18, 2023
+*     INSTRUCTIONS:
+*       Copy & paste the previous PowerPlay for card deck & move all classes to separate .h & .cpp files
+*       Create a Card class w/ members... (private) Suit suit{}; & FaceValue faceValue{}; // (public) Card(Suit suit, FaceValue faceValue);,
+*         void print() const; & move suit & FaceValue enums to this area
+*       Create a Game class w/ members... */
