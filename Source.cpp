dc272577217b8e7e1f238b9429c6fa533c043777
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-21 18:44:14 -0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,18 @@
+/** Author: Izzy Ward*     UALR* 
+* Date:        May 25, 2023*
+* File:        Source.cpp*
+* Purpose:    The purpose of this laboratory is to review very basic Programming I topics*           
+as well as practice downloading, modifying, and submitting a Visual* 
+Studio Project.*/
+#include <iostream> 
+using namespace std;
+int main()
+{
+  cout<<"Hello CPSC 2377"<<endl;
+  cout<<"--My name is Izzy Ward."<<endl;
+  cout<<"--I am from Clarksville, Arkansas."<<endl;
+  cout<<"--I have about 10 months of C++ programming experience."<<endl;
+  cout<<"--I am an avid PC designer and builder."<<endl;
+  system("PAUSE");
+  return 0;
+}
