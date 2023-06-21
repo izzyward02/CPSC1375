774e2aa4c98f2c34d44bd5023fd6480f4910e68a
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-21 01:39:13 +0000

    Create Lvl4.3_PowerPlay1.cpp

---
+++ Lvl4.3_PowerPlay1.cpp
@@ -0,0 +1,5 @@
+/*  Izzy Ward   CPSC 1375   June 20, 2023
+*     INSTRUCTIONS:
+*       Start w/ polygon class from prev. PowerPlay
+*       Modify the polygon class so that the 'calculateDistance' method takes Coordinates */
+
