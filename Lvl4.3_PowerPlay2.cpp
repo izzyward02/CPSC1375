5ef5c4b94deef90e4458922afde18c3d62f8445d
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-21 01:43:17 +0000

    Create Lvl4.3_PowerPlay2.cpp

---
+++ Lvl4.3_PowerPlay2.cpp
@@ -0,0 +1,16 @@
+ /* Izzy Ward   CPSC 1375   June 20, 2023
+ *    INSTRUCTIONS:
+ *      Create three variables: 'float sallysAllowance{ 0.0f };', 'float johnsAllowance{ 0.0f };', and 
+ *        'float* dad{ nullptr };'
+ *      Start a while loop until dad is done, ask dad who he is going to pay allowance to,
+ *      Have dad point at the person he is giving allowance to, ask dad how much, and 
+ *      Add that amount to the person's allowance, using the dad pointer    */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main()
+{
+  return 0;
+}
