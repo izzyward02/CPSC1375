7f8cc70f3624603949c7b82e118504150750c8a9
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-23 02:31:29 +0000

    Update Player.h

--- Player.h
+++ Player.h
@@ -1,3 +1,26 @@
 /* Izzy Ward  CPSC 1375  June 22, 2023
 *    DESCRIPTION:  This program plays a basic Magic: The Gathering game.
 *    It consists of a Card class, Game class, and Player class headers to control the functionality of the game.  */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+class Player
+{
+private:
+	int numCards <= 7;
+	int deckSize <= 60;
+	int playerLife = 20;
+public:
+	Player();
+	Player(int numCards, int deckSize, int playerLife);
+	void firstHand() const;
+	void newHand() const;
+	void drawCards();
+	void displayHand();
+};
+#endif
