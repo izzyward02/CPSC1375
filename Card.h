d285ee2fd485d21f66ab8c835a225f77ad47fa4d
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-23 02:28:43 +0000

    Update Card.h

--- Card.h
+++ Card.h
@@ -1,3 +1,24 @@
 /* Izzy Ward  CPSC 1375  June 22, 2023
 *    DESCRIPTION:  This program plays a basic Magic: The Gathering game.
 *    It consists of a Card class, Game class, and Player class headers to control the functionality of the game.  */
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+#ifndef CARD_H
+#define CARD_H
+
+class Card
+{
+private:
+	std::string cardType[6] = {"Creature", "Enchantment", "Sorcery". "Artifact", "Instant", "Land"};
+        std::string cardColor[6] = {"Blue", "Green", "Red", "Black", "White", "Colorless"};
+	std::string cardType;
+	int manaCost;
+
+public:
+	Card();
+	Card(std::string cardType, std::string cardColor, std::string cardType, int manaCost);
+};
+#endif 
