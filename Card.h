8d75549c02993465120bb9feafa493a248659f66
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-23 02:26:25 +0000

    Create Card.h

---
+++ Card.h
@@ -0,0 +1,3 @@
+/* Izzy Ward  CPSC 1375  June 22, 2023
+*    DESCRIPTION:  This program plays a basic Magic: The Gathering game.
+*    It consists of a Card class, Game class, and Player class headers to control the functionality of the game.  */
