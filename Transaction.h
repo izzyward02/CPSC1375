cfc00cc08183031c2d7a3046b1a316d904c0ea14
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-23 00:39:33 +0000

    Update Transaction.h

--- Transaction.h
+++ Transaction.h
@@ -1,2 +1,21 @@
 //header file for Transaction class inclusion in Lvl 4 Quest
 
+#include <iostream>
+#include <string>
+#include <vector>
+
+class Transaction
+{
+public:	
+	Transaction() = delete;
+	Transaction(Date date, std::string description, float amount);
+	void print() const; //used in Accounts class to display transaction info
+	//Getters
+	float getAmount() const;
+	Date getDate() const;
+	std::string getDescription() const;
+private:
+	Date date();
+	std::string description;
+	float amount;
+};
