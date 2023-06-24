df17833893852fdf0b58ac3d679d769ed809d5e7
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-24 23:18:12 +0000

    Update Card.h

--- Card.h
+++ Card.h
@@ -15,15 +15,19 @@ class Card
 private:
 	std::array cardType[6] = {"Creature", "Enchantment", "Sorcery". "Artifact", "Instant", "Land"};
         std::array cardColor[6] = {"Blue", "Green", "Red", "Black", "White", "Colorless"};
-	std::array cardType;
 	int manaCost;
 
 public:
 	Card();
-	Card(std::array cardType, std::array cardColor, std::array cardType, int manaCost);
+	Card(std::array cardType, std::array cardColor, int manaCost);
 	void tapCard();
 	void untapCard();
-	void movetoExile();
-	void movetoGraveyard();
 };
 #endif 
+
+struct Card
+{
+	CardType cardType{};
+	CardColor cardColor{};
+	void print() const;
+};
