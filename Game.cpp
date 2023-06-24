2b54a7da0cbdfb6684b8abf5c4ecb6ce2e0314df
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-24 23:22:19 +0000

    Create Game.cpp

---
+++ Game.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <random>
+#include <algorithm>
+
+void Game::shuffleDeck()
+{
+	std::random_device seed;
+	std::default_random_engine engine(seed());
+	std::shuffle(drawCard.begin(), drawCard.end(), engine);
+}
+void Game::drawCard(Card drawnCard)
+{
+	currentHand.push_back(drawnCard);
+}
+void Game::displayHand()
+{
+	for (const auto & card : currentHand)
+	{
+		card.print();
+	}
+}
