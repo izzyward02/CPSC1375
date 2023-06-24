1eaddd924e776a36cd3a32c3d9dde4835100f187
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-24 23:19:27 +0000

    Create Card.cpp

---
+++ Card.cpp
@@ -0,0 +1,57 @@
+void Card::shuffleDeck()
+{
+	std::random_device seed;
+	std::default_random_engine engine(seed());
+	std::shuffle(drawCard.begin(), drawCard.end(), engine);
+}
+void Card::drawCard()
+{
+	Card temp{ drawCard.back() };
+	drawCard.pop_back();
+	return temp;
+}
+void Card::print() const
+{
+	switch (cardType)
+	{
+		case CardType::Creature:
+			std::cout << "Creature";
+			break;
+		case CardType::Enchantment:
+			std::cout << "Enchantment";
+			break;
+		case CardType::Sorcery:
+			std::cout << "Sorcery";
+			break;
+		case CardType::Artifact:
+			std::cout << "Artifact";
+			break;
+		case CardType::Instant:
+			std::cout << "Instant";
+			break;
+		case CardType::Land:
+			std::cout << "Land";
+			break;
+	}
+	switch (cardColor)
+	{
+		case CardColor::Blue:
+			std::cout << "Blue";
+			break;
+		case CardColor::Green:
+			std::cout << "Green";
+			break;
+		case CardColor::Red:
+			std::cout << "Red";
+			break;
+		case CardColor::Black:
+			std::cout << "Black";
+			break;
+		case CardColor::White:
+			std::cout << "White";
+			break;
+		case CardColor::Colorless:
+			std::cout << "Colorless";
+			break;
+	}
+}
