331c0e216bc78e6b05bdc8cdcff93c7a33e77fcc
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-24 22:00:07 +0000

    Update Ward_CYOA.cpp

--- Ward_CYOA.cpp
+++ Ward_CYOA.cpp
@@ -12,6 +12,31 @@
 #include <array>
 
 //Card.h
+#include <iostream>
+#include <string>
+#include <vector>
+#include <array>
+
+#ifndef CARD_H
+#define CARD_H
+
+class Card
+{
+private:
+	std::array cardType[6] = {"Creature", "Enchantment", "Sorcery". "Artifact", "Instant", "Land"};
+        std::array cardColor[6] = {"Blue", "Green", "Red", "Black", "White", "Colorless"};
+	std::array cardType;
+	int manaCost;
+
+public:
+	Card();
+	Card(std::array cardType, std::array cardColor, std::array cardType, int manaCost);
+	void tapCard();
+	void untapCard();
+	void movetoExile();
+	void movetoGraveyard();
+};
+#endif 
 
 //Card.cpp
 void Card::shuffleDeck()
@@ -20,6 +45,7 @@ void Card::shuffleDeck()
 	std::default_random_engine engine(seed());
 	std::shuffle(drawCard.begin(), drawCard.end(), engine);
 }
+
 //Game.h
 
 //Game.cpp
