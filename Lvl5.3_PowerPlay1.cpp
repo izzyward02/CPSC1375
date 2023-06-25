10bf88273be3b6acb99e5ef4984229c51077075b
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-25 17:06:11 +0000

    Update Lvl5.3_PowerPlay1.cpp

--- Lvl5.3_PowerPlay1.cpp
+++ Lvl5.3_PowerPlay1.cpp
@@ -3,11 +3,27 @@
 *      Start with given code example for a linked list
 *      Add a new function: node* search(node* head, int key); that searches through a linked list
 *      for a node storing the value that is the same as the key.
-*      It returns a pointer pointing to it if it finds it, else a nullptr.  */
+*      It returns a pointer pointing to it if it finds it, else a nullptr. 
+*      Add a function: void duplicate (node* head, int key); that uses the search function to find
+*      a node with the key, then makes a duplicate of it & adds it to the end of the linked list if found.  */
 
 #include <iostream>
 #include <string>
 #include <vector>
+
+#define nullptr NULL
+using namespace std;
+
+struct node
+{
+  int data;
+  node* next;
+  node* search(node*head, int key)
+  {
+    //if list is empty
+    return nullptr;
+  }
+};
 
 node* search(node* head, int key)
 {
@@ -21,4 +37,9 @@ node* search(node* head, int key)
     currnode = currnode -> next;
   }
   return nullptr;
+}
+
+void duplicate (node* head, int key)
+{
+  
 }
