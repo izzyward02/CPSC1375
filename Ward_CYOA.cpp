d6d77da3d9777a4e77cce9b8b8d93ce0117bb20b
izzyward02 <98180601+izzyward02@users.noreply.github.com>
2023-06-26 21:38:09 +0000

    Update Ward_CYOA.cpp

--- Ward_CYOA.cpp
+++ Ward_CYOA.cpp
@@ -11,194 +11,28 @@
 #include <string>
 #include <array>
 
-//Card.h
-#include <iostream>
-#include <string>
-#include <vector>
-#include <array>
-
-#ifndef CARD_H
-#define CARD_H
-
-class Card
-{
-private:
-	std::array cardType[6] = {"Creature", "Enchantment", "Sorcery". "Artifact", "Instant", "Land"};
-        std::array cardColor[6] = {"Blue", "Green", "Red", "Black", "White", "Colorless"};
-	int manaCost;
-
-public:
-	Card();
-	Card(std::array cardType, std::array cardColor, int manaCost);
-	void tapCard();
-	void untapCard();
-};
-#endif 
-struct Card
-{
-	CardType cardType{};
-	CardColor cardColor{};
-	void print() const;
-};
-
-//Card.cpp
-void Card::shuffleDeck()
-{
-	std::random_device seed;
-	std::default_random_engine engine(seed());
-	std::shuffle(drawCard.begin(), drawCard.end(), engine);
-}
-void Card::drawCard()
-{
-	Card temp{ drawCard.back() };
-	drawCard.pop_back();
-	return temp;
-}
-void Card::print() const
-{
-	switch (cardType)
-	{
-		case CardType::Creature:
-			std::cout << "Creature";
-			break;
-		case CardType::Enchantment:
-			std::cout << "Enchantment";
-			break;
-		case CardType::Sorcery:
-			std::cout << "Sorcery";
-			break;
-		case CardType::Artifact:
-			std::cout << "Artifact";
-			break;
-		case CardType::Instant:
-			std::cout << "Instant";
-			break;
-		case CardType::Land:
-			std::cout << "Land";
-			break;
-	}
-	switch (cardColor)
-	{
-		case CardColor::Blue:
-			std::cout << "Blue";
-			break;
-		case CardColor::Green:
-			std::cout << "Green";
-			break;
-		case CardColor::Red:
-			std::cout << "Red";
-			break;
-		case CardColor::Black:
-			std::cout << "Black";
-			break;
-		case CardColor::White:
-			std::cout << "White";
-			break;
-		case CardColor::Colorless:
-			std::cout << "Colorless";
-			break;
-	}
-}
-
-//Game.h
-#include <iostream>
-#include <string>
-#include <vector>
-
-#ifndef GAME_H
-#define GAME_H
-
-class Game
-{
-private:
-	int lifeCounter {};
-	bool inPlay {};
-	void shuffleDeck {};
-	std::vector<Card> currentHand;
-public:
-	Game();
-	Game(int lifeCounter, bool inPlay, void shuffleDeck, std::vector<Card> currentHand);
-	void discardCard();
-	void drawCard(Card drawnCard);
-	void placeLands();
-	void displayHand();
-	lifeCounter = 20;
-};
-#endif
-
-//Game.cpp
-void Game::shuffleDeck()
-{
-	std::random_device seed;
-	std::default_random_engine engine(seed());
-	std::shuffle(drawCard.begin(), drawCard.end(), engine);
-}
-void Game::drawCard(Card drawnCard)
-{
-	currentHand.push_back(drawnCard);
-}
-void Game::displayHand()
-{
-	for (const auto & card : currentHand)
-	{
-		card.print();
-	}
-}
-
-//Player.h
-#include <iostream>
-#include <string>
-#include <vector>
-
-#ifndef PLAYER_H
-#define PLAYER_H
-
-class Player
-{
-private:
-	int numCards {};
-	int deckSize {};
-	int playerLife {};
-	Game currentHand;
-public:
-	Player();
-	Player(int numCards, int deckSize, int playerLife);
-	void displayHand();
-	void drawCard(Card drawnCard);
-	numCards <= 7;
-	deckSize <= 60;
-	playerLife = 20;
-};
-std::vector<Card> drawHand(std::vector<Card>& deck);
-#endif
-
-//Player.cpp
-std::vector<Card> drawHand(std::vector<Card>& deck)
-{
-	std::vector<Card> hand;
-	hand.reserve(7);
-	for (int i{0}; i < 7; i++)
-	{
-		hand.push_back(deck.back());
-		deck.pop_back();
-	}
-	return hand;
-}
-void Player::drawCard(Card drawnCard)
-{
-	currentHand.drawCard(drawnCard);
-}
-
 //main.cpp
 int main()
 {
 	Card deck;
 	deck.shuffleDeck();
 	Player player1;
+
 	if (drawnCard < 7)
 		player1.drawCard(deck.drawCard());
 	else
 		continue;
+	
+	Game.placeLands();
+	
+	std::cout << "What type of card do you want to play?" << Card.cardType << '\n';
+	std::cin >> cardType;
+	std::cout << "What color is the card?" << Card.cardColor << '\n';
+	std::cin >> cardColor;
+	std::cout << "How much mana does it cost?" << Card.manaCost << '\n';
+	std::cin >> manaCost;
+
+	return 0;
 }
 
 
